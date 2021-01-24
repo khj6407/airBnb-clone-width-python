018ee4a63843311d4d3231f21e8acf4e57541bd7
kimhanju <khj64076407@gmail.com>
2021-01-25 01:00:41 +0900

    create model and admin(user, rooms, photo, core ...etc)

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from core import models as core_models
+from django_countries.fields import CountryField
+from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RommType Model Definition """
+
+    class Meta:
+        verbose_name_plural = "객실유형 관리"
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "편의시설 관리"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "시설 관리"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name_plural = "규칙 관리"
+
+
+# #################### Main Class ####################
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+
+    class Meta:
+        verbose_name_plural = "객실 관리"
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    contury = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    baths = models.IntegerField()
+
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    Facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# #################### Main Class END ####################
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    class Meta:
+        verbose_name_plural = "사진 관리"
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """ Custon User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "USD"
+    CURRENCY_KRW = "KRW"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "usd"), (CURRENCY_KRW, "krw"))
+
+    avatar = models.ImageField("프로필 사진", null=True, blank=True)
+    gender = models.CharField(
+        "성별", choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField("사용자정보", default="", blank=True)
+    birthdate = models.DateField("생년월일", null=True, blank=True)
+    language = models.CharField(
+        "언어유형", choices=LANGUAGE_CHOICES, max_length=5, null=True, blank=True
+    )
+    currency = models.CharField(
+        "화폐유형", choices=CURRENCY_CHOICES, max_length=5, null=True, blank=True
+    )
+    superhost = models.BooleanField("호스트권한", default=False)
